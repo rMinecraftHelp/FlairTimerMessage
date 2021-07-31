c1b594ffb76a7b71b4aa7bc27e9fc0f51be42679
rMinecraftHelp <88217871+rMinecraftHelp@users.noreply.github.com>
2021-07-31 02:40:10 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+username = "MCHelpModTeam"
+password = "[REDACTED]"
+client_id = "[REDACTED]"
+client_secret = "[REDACTED]"
+ 
+#Subreddits
+subreddit = "Minecrafthelp" # "INEEEEDIT" "Ofcoursethatsathing" "All"
+ 
+flair_text = "WFOP" # Case Sensitive
+ 
+interval = 30 # How often should the bot scan the subreddit for these posts, in seconds. Higher = slower/less accurate/save resources, lower = faster/more accurate/use more resources.
+ 
+hours = 168 # How many hours must the flair been on the post to send the notification
+ 
+messagetitle = "Possible abandoned post." # Title of the modmail
+ 
+searchlimit = 800 # Max: 1000, this should only be limited to save on resources. The bot sorts by new and if it isn't catching posts that are being changed to the flair simply because they are too old (say the 301st post on the subreddit is changed to the flair) then increase this limit.mit.
